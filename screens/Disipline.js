457b31a9f5f82b9e765e1cf555a27177bddbdab1
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-02-27 17:15:18 +0100

    refactor: preper a Header section style and data disipline in Disip Componenet

--- screens/Disipline.js
+++ screens/Disipline.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text } from "react-native-paper";
 import { View } from "react-native";
-
-
+import {StyleSheet } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Disipline({ navigation }) {
  
@@ -10,11 +10,36 @@ export default function Disipline({ navigation }) {
   console.log(navigation.getParam("poids"));
   return (
     <View>
-      <View>
-        <Text> hello </Text>
-        <Text> {navigation.getParam("id")} </Text>
+      <View style={styles.Header}>
+        <AntDesign
+          name="arrowleft"
+          size={24}
+          color="black"
+          onPress={() => navigation.navigate("ProjectShow")}
+          style={{ marginBottom: 5 }}
+        />
+
+        <Text style={styles.TitleHeader}>
+          {navigation.getParam("descdisci")}
+        </Text>
       </View>
     </View>
   );
+  
 }
+const styles = StyleSheet.create({
+  Header: {
+    marginBottom: 50,
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+    marginTop: 10,
+    display: "flex",
+    flexDirection: "row",
+  },
+  TitleHeader: {
+    marginLeft: 60,
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
  //navigation.getParam("descdisci"), navigation.getParam("descdisci");
